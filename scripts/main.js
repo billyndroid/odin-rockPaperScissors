d799eae7a64c311c2a0937bb749606607a4973a8
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-01-05 10:00:50 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+let mainArray = ["rock", "paper", "scissors"];
+
 function computerPlay() {
-    
+    const randomArrayElement = Math.floor(Math.random() * 3);
+    return("the computer has selected", randomArrayElement, mainArray[randomArrayElement]);
+       
 }
+
+console.log("the computer has selected", randomArrayElement, mainArray[randomArrayElement]);
