8c3c35e80c3da90c8baef0a7e92f23e33756a5d5
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-01-05 11:16:43 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,25 @@
-let mainArray = ["rock", "paper", "scissors"];
+//randomly select from array and return result
+function computerPlay(arr) {
+    const randomArrayValue = Math.floor(Math.random() * arr.length);
+    const item = arr[randomArrayValue];
+    return item;
+}
+
+let itemArray = ["rock", "paper", "scissors"];
+
+const result = computerPlay(itemArray);
+console.log(result);
+
+// plays one round of game
+function playGame(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a draw. Refresh to play again.";
+    } if (playerSelection == "rock" && computerSelection == "paper") {
+        return "paper wins. Refresh to try again.";
+    } if (playerSelection == "")
+}
+
+/*let mainArray = ["rock", "paper", "scissors"];
 
 function computerPlay() {
     let mainArray = ["rock", "paper", "scissors"];
@@ -7,4 +28,4 @@ function computerPlay() {
        
 }
 
-console.log("the computer has selected", randomArrayElement, mainArray[randomArrayElement]);
+//console.log("the computer has selected", randomArrayElement, mainArray[randomArrayElement]);*/
