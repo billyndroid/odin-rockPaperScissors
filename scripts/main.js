c30bb3182b81f3c49fcfba36f41708322df8b70e
billyndroid <70548591+billyndroid@users.noreply.github.com>
2022-01-05 10:41:49 +0000

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 let mainArray = ["rock", "paper", "scissors"];
 
 function computerPlay() {
+    let mainArray = ["rock", "paper", "scissors"];
     const randomArrayElement = Math.floor(Math.random() * 3);
     return("the computer has selected", randomArrayElement, mainArray[randomArrayElement]);
        
